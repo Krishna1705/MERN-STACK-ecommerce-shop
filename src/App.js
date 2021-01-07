3b1fed4f9741c28ec2af790067e7f87b6b85f323
Krishna <ridpatel2907@gmail.com>
2021-01-07 19:09:29 +0700

    just have moved my code from normal html template to react app,and created and store data in front-end,fetched data from front end,and render it on the home page of the shop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import  {data} from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div className="grid-container">
+                <header className="row">
+                        <div>
+                            <a className="brand" href="/">
+                                ARIHANT
+                            </a>
+                        </div>
+                        <div>
+                         <a href="/cart"><button>Cart</button></a>
+                            <a href="/signin"><button>Signin</button> </a>
+                        </div>     
+                </header>
+
+                <main>
+                <div className="row center">
+                      {
+                        data.products.map((product,index)=>(
+                            <>
+                                <div key={product._id} className="card">
+                                <a href="product.html"><img className="medium" src={product.image} alt="product"/></a>
+                                <div className="card-body">
+                                                  <a href="#">
+                                                      <h2>{product.name}</h2>  
+                                                  </a>
+                                                  <div className="rating">
+                                                  {product.rating}
+                                                      <span><i className="fa fa-star"></i></span>
+                                                      <span><i className="fa fa-star"></i></span>
+                                                      <span><i className="fa fa-star"></i></span>
+                                                      <span><i className="fa fa-star"></i></span>
+                                                      <span><i className="fa fa-star"></i></span>
+                                                  </div>
+                                                  <div className="price">
+                                                      ${product.price}
+                                                  </div>
+                                              </div>
+                                          </div>
+                            </>
+                          ))
+                      }            
+                </div>           
+                </main>
+
+                <footer className="row center">
+                        All rights reserved
+                </footer>
+        </div>
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,70 @@
+export const data={
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'./images/p2.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Product'
+        },
+        {
+            _id:'2',
+            name:'Adidas Slim Shirt',
+            category:'Shirts',
+            image:'./images/p2.jpg',
+            price:125,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Product'
+        },
+        {
+            _id:'3',
+            name:'Puma Slim Shirt',
+            category:'Shirts',
+            image:'./images/p2.jpg',
+            price:69,
+            brand:'Puma',
+            rating:4.5,
+            numReviews:5,
+            description:'High Quality Product'
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Pants',
+            category:'Pants',
+            image:'./images/p2.jpg',
+            price:105,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:8,
+            description:'High Quality Product'
+        },
+        {
+            _id:'5',
+            name:'Adidas Pants',
+            category:'Pants',
+            image:'./images/p2.jpg',
+            price:110,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Product'
+        },
+        {
+            _id:'6',
+            name:'Puma Pants',
+            category:'Pants',
+            image:'./images/p2.jpg',
+            price:120,
+            brand:'Puma',
+            rating:4,
+            numReviews:1,
+            description:'High Quality Product'
+        },
+    ]
+}
