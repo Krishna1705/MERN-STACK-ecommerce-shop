faa4f630ed65273d2abc003e4367d04f0c37657c
Krishna <ridpatel2907@gmail.com>
2021-01-08 19:20:06 +0700

    added data.js file inside backend folder,and added code to server.js file,also installed nodemon,also changed scripts inside package.json file to enable nodemon

---
+++ backend/data.js
@@ -0,0 +1,76 @@
+export const data={
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'./images/p2.jpg',
+            price:120,
+            brand:'Nike',
+            rating:4,
+            numReviews:10,
+            description:'High Quality Product',
+            countInStock:10
+        },
+        {
+            _id:'2',
+            name:'Adidas Slim Shirt',
+            category:'Shirts',
+            image:'./images/p2.jpg',
+            price:125,
+            brand:'Adidas',
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Product',
+            countInStock:20
+        },
+        {
+            _id:'3',
+            name:'Puma Slim Shirt',
+            category:'Shirts',
+            image:'./images/p2.jpg',
+            price:69,
+            brand:'Puma',
+            rating:3.5,
+            numReviews:5,
+            description:'High Quality Product',
+            countInStock:50
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Pants',
+            category:'Pants',
+            image:'./images/p2.jpg',
+            price:105,
+            brand:'Nike',
+            rating:3,
+            numReviews:8,
+            description:'High Quality Product',
+            countInStock:80
+        },
+        {
+            _id:'5',
+            name:'Adidas Pants',
+            category:'Pants',
+            image:'./images/p2.jpg',
+            price:110,
+            brand:'Adidas',
+            rating:3.2,
+            numReviews:10,
+            description:'High Quality Product',
+            countInStock:4
+        },
+        {
+            _id:'6',
+            name:'Puma Pants',
+            category:'Pants',
+            image:'./images/p2.jpg',
+            price:120,
+            brand:'Puma',
+            rating:1.6,
+            numReviews:1,
+            description:'High Quality Product',
+            countInStock:0
+        },
+    ]
+}
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 import express from 'express';
+import {data} from './data.js'
 //above line can be used after new ES6 feature import
 //for this we need to write "type":"module" inside package.json file
 //var express = require('express');
 var app = express();
 
+app.get('/api/products',(req,res)=>{
+    res.send(data.products)
+})
+
 app.get('/', function (req, res) {
     res.send('kaju....server is ready');
   })
 
-  app.listen(5000,()=>{
-      console.log("server is running at http://localhost:5000");
+  const port=process.env.PORT || 5000;
+  app.listen(port,()=>{
+      console.log(`server is running at http://localhost:${port}`);
   })
