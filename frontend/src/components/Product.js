b991162c691b31d4a8788cdc22b3cd569a6f09a7
Krishna <ridpatel2907@gmail.com>
2021-01-08 23:46:27 +0700

    defined proxy inside package.json file of frontend,commented data.js file from frontend code,then fetched products data from backend data.js file to the frontend Homescreen with help of axios

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -20,8 +20,8 @@ export default function Product({product}){
                                                   <div className="price">
                                                       ${product.price}
                                                   </div>
-                                              </div>
-                                          </div>
+                                </div>
+         </div>
 
         </>
     );
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import  {data} from '../data'
+import React,{useState,useEffect} from 'react';
+//import  {data} from '../data';
 import Product from '../components/Product';
+import axios from 'axios';
+
 
 export default function HomeScreen() {
+    const [products,setProducts]=useState([]);
+    const [loading,setLoading]=useState(false);
+    const [error,setError]=useState(false);
+
+    useEffect(() => {
+        const fetchedData=async () => {
+                              
+                                try{ 
+                                      setLoading(true);
+                                      const response=await axios.get('/api/products');
+                                      const responseData=await response.data;
+                                      console.log(responseData);
+                                      setLoading(false);
+                                      setProducts(responseData);
+                                    }catch(error){
+                                         console.log(error)
+                                         setError(error.message);
+                                         setLoading(false);
+                                        }
+                            }
+        fetchedData();
+    }, []);
+
     return (
-        <div className="row center">
-            {
-            data.products.map((product)=>(
-                <>
-                    <Product  key={product._id} product={product}></Product>
-                </>
-                ))
-            }            
-       </div>       
+        <>
+        {loading?(<h1>loading...</h1>)
+        :error?({error})
+        :(
+            <div className="row center">
+                {
+                    products.map(product=>(
+                                    <Product key={product._id} product={product}></Product>
+                                     ))
+                }
+           </div>   
+        )}
+       </>    
     )
 }
